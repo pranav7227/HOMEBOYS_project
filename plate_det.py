8f98c1a493c1757c8f8cf9de9fbb61f9e262dca9
pranav7227 <144687661+pranav7227@users.noreply.github.com>
2024-01-20 12:34:28 +0530

    Update plate_det.py

--- plate_det.py
+++ plate_det.py
@@ -1,10 +1,1 @@
-import easyocr
-import cv2
-import imutils
-import numpy as np , pandas as pd
-from matplotlib import pyplot as plt 
-import os
 
-image_path = os.path.join(r"C:\Users\LENOVO\Desktop\images (3).jpg")
-if os.path.exists(image_path):
-   print(x)
